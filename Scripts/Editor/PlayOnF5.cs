004cf9b4a899c1aff0eebd77082e35b4c5bc96bd
Christian Stockinger <christian.stockinger@intive.com>
2021-10-12 09:56:52 +0200

    add Editor Script PlayOnF5

---
+++ Scripts/Editor/PlayOnF5.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Packages.Editor
+{
+    /// <summary>
+    /// This class is used to start the PlayMode with F5. As it is done in VisualStudio.
+    /// </summary>
+    public class PlayOnF5 : ScriptableObject
+    {
+        private const string PLAY_MENU_ITEM_PATH = "Edit/Play";
+
+        [MenuItem("Edit/Run _F5")]
+        private static void TogglePlayMode()
+        {
+            if (!Application.isPlaying)
+            {
+                EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+            }
+            EditorApplication.ExecuteMenuItem(PLAY_MENU_ITEM_PATH);
+        }
+    }
+}
