ac0fc4ce6eaa94f844f523b62bef71f7dda97799
Christian Stockinger <christian.stockinger@intive.com>
2021-10-21 09:09:48 +0200

    Add RuntimePlatformListener and SceneReference

---
+++ Scripts/Utils/RuntimePlatformListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace BaseTools.Utils
+{
+    public class RuntimePlatformListener : SerializedMonoBehaviour
+    {
+        public Dictionary<RuntimePlatform, UnityEvent> PlatformEvents;
+        [Tooltip("Gets Fired if the Current Platform is not Selected int the BuildPlatformEvents")]
+        public UnityEvent OtherPlatformEvent;
+    
+        void Awake()
+        {
+            if(PlatformEvents.ContainsKey(Application.platform))
+                PlatformEvents[Application.platform].Invoke();
+            else
+                OtherPlatformEvent.Invoke();
+        }
+    }
+}
+
